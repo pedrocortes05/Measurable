8d48e9e7a5a018966ebbf7826349af8c8e24443e
Pedro <pedrocortespro@gmail.com>
2023-09-24 06:32:23 -0600

    UI fully functional

--- Unity project/Assets/GameManager.cs
+++ Unity project/Assets/GameManager.cs
@@ -7,45 +7,86 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public List<GameObject> UserInterfaces;
-    public GameObject item;
-    public GameObject content;
+    public Button goToMenuButton;
+    public Button goToMenuButton2;
     public Button goToItemsButton;
     public Button goToBinsButton;
-    public Button addItemButton;
-    public Button addBinButton;
+    public Button goToItemFormButton;
+    public Button goToBinFormButton;
+    public Button ordenarButton;
+    //public Button addBinButton;
     public List<ItemModel> Items;
     public List<BinModel> Bins;
+
+    PackerModel packer = new PackerModel();
 
     // Start is called before the first frame update
     void Start()
     {
         // Hide UIs and activate main UI
-        UnhideUI(UserInterfaces[0]);
+        UnHideUI(UserInterfaces[0]);
 
-        goToItemsButton.GetComponent<Button>().onclick.AddListener(UnhideUI(UserInterfaces[1]));
-        //goToBinsButton.GetComponent<Button>().onclick.AddListener(UnhideUI(UserInterfaces[1]));
+        goToMenuButton.GetComponent<Button>().onClick.AddListener(GoToMenu);
+        goToItemsButton.GetComponent<Button>().onClick.AddListener(GoToItems);
+        goToBinsButton.GetComponent<Button>().onClick.AddListener(GoToBins);
+        goToItemFormButton.GetComponent<Button>().onClick.AddListener(GoToItemForm);
+        goToBinFormButton.GetComponent<Button>().onClick.AddListener(GoToBinForm);
+        goToMenuButton2.GetComponent<Button>().onClick.AddListener(GoToMenu);
+        ordenarButton.GetComponent<Button>().onClick.AddListener(trySort);
 
-        Button addItem = addItemButton.GetComponent<Button>();
-        addItem.onClick.AddListener(CreateItem);
     }
 
-    void UnhideUI(GameObject active_UI)
+    void UnHideUI(GameObject active_UI)
     {
         foreach (GameObject UI in UserInterfaces)
         {
-            HideUI(UI);
+            UI.SetActive(false);
         }
         active_UI.SetActive(true);
     }
 
-    void HideUI(GameObject UI)
+    public void trySort()
     {
-        UI.SetActive(false);
-    }
+        packer.AddBin(Bins[0]);
+        foreach (ItemModel item in Items)
+        {
+            packer.AddItem(item);
+        }
 
-    void CreateItem()
-    {
-        Instantiate(item).transform.parent = content.transform;
+        packer.Pack();
+
+        foreach (ItemModel im in Bins[0].Items)
+        {
+            Debug.Log(im.String());
+        }
+        // Debug.Log("------");
+        // foreach (BinModel bin in Bins)
+        // {
+        //     Debug.Log(bin.Name);
+        //     Debug.Log(bin.Width);
+        //     Debug.Log(bin.Height);
+        //     Debug.Log(bin.Depth);
+        //     Debug.Log(bin.MaxWeight);
+        //     Debug.Log("-");
+
+        // }
+        // Debug.Log("------");
+        // foreach (ItemModel item in Items)
+        // {
+        //     Debug.Log(item.Name);
+        //     Debug.Log(item.Width);
+        //     Debug.Log(item.Height);
+        //     Debug.Log(item.Depth);
+        //     Debug.Log(item.Weight);
+        //     Debug.Log("-");
+
+        // }
+        // Debug.Log("------");
     }
+    public void GoToMenu() { UnHideUI(UserInterfaces[0]); }
+    public void GoToItems() { UnHideUI(UserInterfaces[1]); }
+    public void GoToItemForm() { UnHideUI(UserInterfaces[2]); }
+    public void GoToBins() { UnHideUI(UserInterfaces[3]); }
+    public void GoToBinForm() { UnHideUI(UserInterfaces[4]); }
 
 }
--- Unity project/Assets/SaveBin.cs
+++ Unity project/Assets/SaveBin.cs
@@ -5,38 +5,38 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class SaveItem : MonoBehaviour
+public class SaveBin : MonoBehaviour
 {
     public GameManager gameManager;
-    public TMP_InputField itemName;
+    public TMP_InputField binName;
     public TMP_InputField length;
     public TMP_InputField width;
     public TMP_InputField height;
     public TMP_InputField weight;
-    public GameObject itemIcon;
+    public GameObject binIcon;
     public GameObject content;
-    public Button newItemButton;
+    public Button newBinButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        newItemButton.GetComponent<Button>().onClick.AddListener(saveItem);
+        newBinButton.GetComponent<Button>().onClick.AddListener(saveBin);
     }
 
-    void saveItem()
+    void saveBin()
     {
-        ItemModel item = new ItemModel(itemName.text, double.Parse(length.text), double.Parse(width.text), double.Parse(height.text), double.Parse(weight.text));
+        BinModel bin = new BinModel(binName.text, double.Parse(length.text), double.Parse(width.text), double.Parse(height.text), double.Parse(weight.text));
         
-        GameObject currentItem = Instantiate(itemIcon);
+        GameObject currentItem = Instantiate(binIcon);
         currentItem.transform.parent = content.transform;
         // currentItem.GetComponent<itemRef>().text1.GetComponent<TextMeshPro>().text = Convert.ToString(itemName);
         // currentItem.GetComponent<itemRef>().text2.GetComponent<TextMeshPro>().text = Convert.ToString(length) + " x " + Convert.ToString(width) + " x " + Convert.ToString(height) + ", " + Convert.ToString(weight) + " kg";
 
-        currentItem.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = itemName.text;
+        currentItem.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = binName.text;
         currentItem.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = length.text + " x " + width.text + " x " + height.text + ", " + weight.text + " kg";
         
-        gameManager.Items.Add(item);
+        gameManager.Bins.Add(bin);
 
-        gameManager.GoToItems();
+        gameManager.GoToBins();
     }
 }
