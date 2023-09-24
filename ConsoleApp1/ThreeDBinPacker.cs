7f46588e192b0fa223a298ec8a8785dcbb4d7b48
Pedro <pedrocortespro@gmail.com>
2023-09-23 20:40:56 -0600

    finished library, going to delete unnecesarry decimal conversions

--- ConsoleApp1/ThreeDBinPacker.cs
+++ ConsoleApp1/ThreeDBinPacker.cs
@@ -11,29 +11,29 @@ public class ThreeDBinPacker
 // CONSTANTS
 public class RotationType
 {
-    public static readonly int RT_WHD = 0;
-    public static readonly int RT_HWD = 1;
-    public static readonly int RT_HDW = 2;
-    public static readonly int RT_DHW = 3;
-    public static readonly int RT_DWH = 4;
-    public static readonly int RT_WDH = 5;
-    public static readonly int[] ALL = new int[] { RT_WHD, RT_HWD, RT_HDW, RT_DHW, RT_DWH, RT_WDH };
+    public const int RT_WHD = 0;
+    public const int RT_HWD = 1;
+    public const int RT_HDW = 2;
+    public const int RT_DHW = 3;
+    public const int RT_DWH = 4;
+    public const int RT_WDH = 5;
+    public static List<int> ALL = new List<int>() { RT_WHD, RT_HWD, RT_HDW, RT_DHW, RT_DWH, RT_WDH };
 }
 
 public class Axis
 {
-    public static readonly int WIDTH = 0;
-    public static readonly int HEIGHT = 1;
-    public static readonly int DEPTH = 2;
-    public static readonly int[] ALL = new int[] { WIDTH, HEIGHT, DEPTH };
+    public const int WIDTH = 0;
+    public const int HEIGHT = 1;
+    public const int DEPTH = 2;
+    public static List<int> ALL = new List<int>() { WIDTH, HEIGHT, DEPTH };
 }
 
 public class AuxilaryMethods
 {
-    public bool RectIntersect(ItemModel item1, ItemModel item2, int x, int y)
+    public static bool RectIntersect(ItemModel item1, ItemModel item2, int x, int y)
     {
-        double d1 = item1.GetDimension();
-        double d2 = item2.GetDimension();
+        List<double> d1 = item1.GetDimension();
+        List<double> d2 = item2.GetDimension();
 
         double cx1 = item1.Position[x] + d1[x]/2;
         double cy1 = item1.Position[y] + d1[y]/2;
@@ -46,23 +46,23 @@ public class AuxilaryMethods
         return ix < (d1[x]+d2[x])/2 && iy < (d1[y]+d2[y])/2;
     }
 
-    public bool Intersect(ItemModel item1, ItemModel item2)
+    public static bool Intersect(ItemModel item1, ItemModel item2)
     {
         return (
-            AuxilaryMethods.RectIntersect(item1, item2, Axis.WIDTH, Axis.HEIGHT) &&
-            AuxilaryMethods.RectIntersect(item1, item2, Axis.HEIGHT, Axis.DEPTH) &&
-            AuxilaryMethods.RectIntersect(item1, item2, Axis.WIDTH, Axis.DEPTH)
+            RectIntersect(item1, item2, Axis.WIDTH, Axis.HEIGHT) &&
+            RectIntersect(item1, item2, Axis.HEIGHT, Axis.DEPTH) &&
+            RectIntersect(item1, item2, Axis.WIDTH, Axis.DEPTH)
         );
     }
 
-    public double GetLimitNumberOfDecimals(int numberOfDecimals)
+    public static double GetLimitNumberOfDecimals(int numberOfDecimals)
     {
-        return Decimal("1." +  + new string('0', numberOfDecimals)); // Decimal doesnt exist yet
+        return Decimal("1." + new string('0', numberOfDecimals)); // Decimal doesnt exist yet
     }
 
-    public double SetToDecimal(double value, int numberOfDecimals)
+    public static double SetToDecimal(double value, int numberOfDecimals)
     {
-        numberOfDecimals = AuxilaryMethods.GetLimitNumberOfDecimals(numberOfDecimals);
+        numberOfDecimals = GetLimitNumberOfDecimals(numberOfDecimals);
         return Decimal(value).quantize(numberOfDecimals);  // What is quantize?
     }
 }
@@ -71,20 +71,29 @@ public class AuxilaryMethods
 public static class Globals
 {
     public static readonly int DEFAULT_NUMBER_OF_DECIMALS = 3;
-    public static readonly int[] START_POSITION = new int[] { 0, 0, 0 };
+    public static readonly List<double> START_POSITION = new List<double>() { 0, 0, 0 };
 }
 
 // MODELS
 public class ItemModel
 {
-    public ItemModel(string name, int width, int height, int depth, int weight)
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public double Depth { get; set; }
+    public double Weight { get; set; }
+    public int Rotation { get; set; }
+    public List<double> Position { get; set; }
+    public int NumberOfDecimals { get; set; }
+
+    public ItemModel(string name, double width, double height, double depth, double weight)
     {
         Name = name;
         Width = width;
         Height = height;
         Depth = depth;
         Weight = weight;
-        RotationType = 0;
+        Rotation = 0;
         Position = Globals.START_POSITION;
         NumberOfDecimals = Globals.DEFAULT_NUMBER_OF_DECIMALS;
     }
@@ -103,31 +112,40 @@ public class ItemModel
         return AuxilaryMethods.SetToDecimal(Width * Height * Depth, NumberOfDecimals);
     }
 
-    public double GetDimension()
+    public List<double> GetDimension()
     {
-        switch(RotationType)
+        switch(Rotation)
         {
             case RotationType.RT_WHD:
-                return new double[] { Width, Height, Depth };
+                return new List<double>() { Width, Height, Depth };
             case RotationType.RT_HWD:
-                return new double[] { Height, Width, Depth };
+                return new List<double>() { Height, Width, Depth };
             case RotationType.RT_HDW:
-                return new double[] { Height, Depth, Width };
+                return new List<double>() { Height, Depth, Width };
             case RotationType.RT_DHW:
-                return new double[] { Depth, Height, Width };
+                return new List<double>() { Depth, Height, Width };
             case RotationType.RT_DWH:
-                return new double[] { Depth, Width, Height };
+                return new List<double>() { Depth, Width, Height };
             case RotationType.RT_WDH:
-                return new double[] { Width, Depth, Height };
+                return new List<double>() { Width, Depth, Height };
             default:
-                return new double[] { };
+                return new List<double>();
         }
     }
 }
 
 public class BinModel
 {
-    public BinModel(string name, int width, int height, int depth, int maxWeight)
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public double Depth { get; set; }
+    public double MaxWeight { get; set; }
+    public List<ItemModel> Items { get; set; }
+    public List<ItemModel> UnfittedItems { get; set; }
+    public int NumberOfDecimals { get; set; }
+
+    public BinModel(string name, double width, double height, double depth, double maxWeight)
     {
         Name = name;
         Width = width;
@@ -165,16 +183,16 @@ public class BinModel
         return AuxilaryMethods.SetToDecimal(totalWeight, NumberOfDecimals);
     }
 
-    public bool PutItem(ItemModel item, List<int> pivot)
+    public bool PutItem(ItemModel item, List<double> pivot)
     {
         bool fit = false;
-        List<int> validItemPosition = item.Position;
+        List<double> validItemPosition = item.Position;
         item.Position = pivot;
 
-        for (int i = 0; i < RotationType.ALL.Length; i++)
+        for (int i = 0; i < RotationType.ALL.Count; i++)
         {
-            item.RotationType = i;
-            double[] dimension = item.GetDimension();
+            item.Rotation = i;
+            List<double> dimension = item.GetDimension();
             if (
                 Width < pivot[0] + dimension[0] ||
                 Height < pivot[1] + dimension[1] ||
@@ -225,6 +243,11 @@ public class BinModel
 
 public class PackerModel
 {
+    public List<BinModel> Bins { get; set; }
+    public List<ItemModel> Items { get; set; }
+    public List<ItemModel> UnfitItems { get; set; }
+    public int TotalItems { get; set; }
+
     public PackerModel()
     {
         Bins = new List<BinModel>();
@@ -233,24 +256,24 @@ public class PackerModel
         TotalItems = 0;
     }
 
-    public List<BinModel> AddBin(BinModel bin)
+    public void AddBin(BinModel bin)
     {
-        return Bins.Add(bin);
+        Bins.Add(bin);
     }
 
-    public List<ItemModel> AddItem(ItemModel item)
+    public void AddItem(ItemModel item)
     {
-        TotalItems = Items.Length + 1;
-        return Items.Add(item);
+        TotalItems = Items.Count + 1;
+        Items.Add(item);
     }
 
-    public void PackToBin(List<BinModel> bin, List<ItemModel> item)
+    public void PackToBin(BinModel bin, ItemModel item)
     {
         bool fitted = false;
 
-        if (!bin.Items)
+        if (bin.Items.Count == 0)
         {
-            response = bin.PutItem(item, Globals.START_POSITION);
+            bool response = bin.PutItem(item, Globals.START_POSITION);
 
             if (!response)
             {
@@ -262,16 +285,18 @@ public class PackerModel
 
         for (int axis = 0; axis < 3; axis++)
         {
-            itemsInBin = bin.Items;
+            List<ItemModel> itemsInBin = bin.Items;
 
             foreach (ItemModel ib in itemsInBin)
             {
-                int[] pivot = new int[] { 0, 0, 0 };
-                double w, h, d;
-                ib.GetDimension(out w, out h, out d);
+                List<double> pivot = new List<double>() { 0, 0, 0 };
+                List<double> dimensions = ib.GetDimension();
+                double w = dimensions[0];
+                double h = dimensions[1];
+                double d = dimensions[2];
                 if (axis == Axis.WIDTH)
                 {
-                    int[] pivot = new int[] { 
+                    pivot = new List<double>() { 
                         ib.Position[0] + w,
                         ib.Position[1],
                         ib.Position[2]
@@ -279,7 +304,7 @@ public class PackerModel
                 }
                 else if (axis == Axis.HEIGHT)
                 {
-                    int[] pivot = new int[] { 
+                    pivot = new List<double>() { 
                         ib.Position[0],
                         ib.Position[1] + h,
                         ib.Position[2]
@@ -287,7 +312,7 @@ public class PackerModel
                 }
                 else if (axis == Axis.DEPTH)
                 {
-                    int[] pivot = new int[] { 
+                    pivot = new List<double>() { 
                         ib.Position[0],
                         ib.Position[1],
                         ib.Position[2] + d
@@ -312,7 +337,7 @@ public class PackerModel
         }
     }
 
-    public void Pack(bool biggerFirst = false, bool distributeItems = false, int numberOfDecimals = Globals.DEFAULT_NUMBER_OF_DECIMALS)
+    public void Pack(bool biggerFirst = false, bool distributeItems = false, int numberOfDecimals = 3) //Globals.DEFAULT_NUMBER_OF_DECIMALS
     {
         foreach (BinModel bin in Bins)
         {
