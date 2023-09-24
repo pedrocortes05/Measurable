547574081072b80463001480e174adbf8875f560
Pedro <pedrocortespro@gmail.com>
2023-09-23 18:17:02 -0600

    created item and bin model

--- ConsoleApp1/ThreeDBinPacker.cs
+++ ConsoleApp1/ThreeDBinPacker.cs
@@ -71,4 +71,88 @@ public class BinModel
         UnfittedItems = new List<ItemModel>();
         NumberOfDecimals = DEFAULT_NUMBER_OF_DECIMALS;
     }
+
+    public void FormatNumbers(int numberOfDecimals)
+    {
+        Width = SetToDecimal(Width, NumberOfDecimals);
+        Height = SetToDecimal(Height, NumberOfDecimals);
+        Depth = SetToDecimal(Depth, NumberOfDecimals);
+        MaxWeight = SetToDecimal(MaxWeight, NumberOfDecimals);
+        NumberOfDecimals = numberOfDecimals;
+    }
+
+    public double GetVolume()
+    {
+        return SetToDecimal(Width * Height * Depth, NumberOfDecimals);
+    }
+
+    public double GetTotalWeight()
+    {
+        double totalWeight = 0;
+
+        foreach (ItemModel item in Items)
+        {
+            totalWeight += item.Weight;
+        }
+
+        return SetToDecimal(totalWeight, NumberOfDecimals);
+    }
+
+    public bool PutItem(ItemModel item, List<int> pivot)
+    {
+        bool fit = false;
+        List<int> validItemPosition = item.Position;
+        item.Position = pivot;
+
+        for (int i = 0; i < RotationType.ALL.Length; i++)
+        {
+            item.RotationType = i;
+            double[] dimension = item.GetDimension();
+            if (
+                Width < pivot[0] + dimension[0] ||
+                Height < pivot[1] + dimension[1] ||
+                Depth < pivot[2] + dimension[2]
+            )
+            {
+                continue;
+            }
+
+            fit = true;
+
+            foreach (ItemModel currentItemInBin in Items)
+            {
+                if (Intersect(currentItemInBin, item))
+                {
+                    fit = false;
+                    break;
+                }
+            }
+
+            if (fit)
+            {
+                if (GetTotalWeight() + item.Weight > MaxWeight)
+                {
+                    fit = false;
+                    return fit;
+                }
+
+                Items.Add(item);
+            }
+
+            if (!fit)
+            {
+                item.Position = validItemPosition;
+            }
+
+            return fit;
+        }
+
+        if (!fit)
+        {
+            item.Position = validItemPosition;
+        }
+
+        return fit;
+    }
 }
+
